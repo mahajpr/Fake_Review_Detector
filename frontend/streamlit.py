0f75b19b43548d54bb0566c49eba596d25e8f3bd
JPR278 <mahavigneshwari8@gmail.com>
2026-03-31 19:58:51 +0530

    frontend and database changed

--- frontend/streamlit.py
+++ frontend/streamlit.py
@@ -7,7 +7,7 @@ st.set_page_config(
     layout="wide"
 )
 
-BASE_URL = "https://fakereviewdetector-production.up.railway.app"
+BASE_URL = "http://127.0.0.1:8000"
 
 API_ANALYZE = f"{BASE_URL}/analyze"
 API_STATS = f"{BASE_URL}/stats"
